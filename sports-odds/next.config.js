ee8e395c539a52c44e1550c7aa6f1839eebda9d4
Reaveskev <Reaveskev@gmail.com>
2023-03-31 16:01:39 -0700

    Messing with config file

--- sports-odds/next.config.js
+++ sports-odds/next.config.js
@@ -1,24 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   module: {
-//     rules: [
-//       {
-//         test: /\.csv$/,
-//         loader: "csv-loader",
-//         options: {
-//           dynamicTyping: true,
-//           header: true,
-//           skipEmptyLines: true,
-//         },
-//       },
-//     ],
-//   },
-// };
+/** @type {import('next').NextConfig} */
 
-// module.exports = nextConfig;
-
-module.exports = {
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
   trailingSlash: true,
   webpack: (config, options) => {
     config.module.rules.push({
@@ -55,3 +39,43 @@ module.exports = {
     };
   },
 };
+
+module.exports = nextConfig;
+
+// module.exports = {
+//   trailingSlash: true,
+//   webpack: (config, options) => {
+//     config.module.rules.push({
+//       test: /\.csv$/,
+//       loader: "csv-loader",
+//       options: {
+//         dynamicTyping: true,
+//         header: true,
+//         skipEmptyLines: true,
+//       },
+//     });
+
+//     return config;
+//   },
+//   images: {
+//     loader: "akamai",
+//     path: "/",
+//   },
+//   exportPathMap: async function (
+//     defaultPathMap,
+//     { dev, dir, outDir, distDir, buildId }
+//   ) {
+//     return {
+//       "/": { page: "/" },
+//       "/NFL": { page: "/NFL" },
+//       "/NBA": { page: "/NBA" },
+//       "/NHL": { page: "/NHL" },
+//       "/WNBA": { page: "/WNBA" },
+//       "/CFB": { page: "/CFB" },
+//       "/Soccer": { page: "/Soccer" },
+//       "/MLB": { page: "/MLB" },
+//       "/MCBB": { page: "/MCBB" },
+//       "/WCBB": { page: "/WCBB" },
+//     };
+//   },
+// };
