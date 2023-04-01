86182c8f3cd565d2e904aeb02f6353d49f51ad8a
Reaveskev <Reaveskev@gmail.com>
2023-03-31 20:12:07 -0700

    Flask rework:

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ else:
 @app.route("/", defaults={'path': ''})  
 @app.route('/<path:path>')
 def catch_all(path):
+    print(path)
     if path != "" and os.path.exists(app.static_folder + '/' + path):
         return send_from_directory(app.static_folder, path)
     else:
