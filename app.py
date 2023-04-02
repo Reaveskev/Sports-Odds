9afa6eeec6e804ef0046dcefc6b7c412fdc71cf3
Reaveskev <Reaveskev@gmail.com>
2023-04-01 23:36:49 -0700

    Migrating

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 from flask_mysqldb import MySQL
 from bs4 import BeautifulSoup
 import MySQLdb.cursors
-# import mysql.connector
+from hidden import *
 import requests
 import os
 
@@ -17,12 +17,13 @@ CORS(app, support_credentials=True)
 # MySql ####################
 # app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_HOST'] = os.environ['CLEARDB_DATABASE_URL']
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Upshaw123!'
-app.config['MYSQL_DB'] = 'sports_odds'
+app.config['MYSQL_USER'] = db_user
+app.config['MYSQL_PASSWORD'] = db_password
+app.config['MYSQL_DB'] = db_name
  
 mysql = MySQL(app)
 ####################
+
 
 
 #####################
