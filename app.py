9fad2b38578ae12c080dfa351e200964d12901ca
Reaveskev <Reaveskev@gmail.com>
2023-04-02 17:01:11 -0700

    Updating .env

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ CORS(app, support_credentials=True)
 
 # MySql ####################
 # app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
  
 mysql = MySQL(app)
 ####################
