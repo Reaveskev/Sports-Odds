712ea881b9ce9f9a73125c240353280a434efe5d
Reaveskev <Reaveskev@gmail.com>
2023-04-19 12:38:51 -0700

    hmm

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_mysqldb import MySQL
 from bs4 import BeautifulSoup
 import mysql.connector
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
 # import MySQLdb.cursors
 import requests
 import os
@@ -27,14 +27,12 @@ cors = CORS(app, support_credentials=True)
 # options.add_argument("--disable-dev-shm-usage")
 # driver = webdriver.Chrome(options=options)
 
-
-
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--disable-dev-shm-usage")
-# chrome_options.add_argument("--no-sandbox")
-# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 
 
@@ -276,14 +274,14 @@ def scrape_Odds(league):
     url = 'https://sports.yahoo.com/{}/odds/'.format(league)
 
    
-    # driver.get(url)
-    # driver.implicitly_wait(2)
-    # html = driver.page_source 
+    driver.get(url)
+    driver.implicitly_wait(5)
+    html = driver.page_source 
 
-    page_to_scrape = requests.get(url)
+    # page_to_scrape = requests.get(url)
 
-    soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-    # soup = BeautifulSoup(html, "html.parser")
+    # soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+    soup = BeautifulSoup(html, "html.parser")
 
     upcoming_games = []
 
