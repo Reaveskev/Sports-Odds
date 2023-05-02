01266257dd8a2e86c9a309dd271ba7ecb1df05e4
Reaveskev <Reaveskev@gmail.com>
2023-05-01 18:48:20 -0700

    fixing timezone

--- app.py
+++ app.py
@@ -37,11 +37,19 @@ cors = CORS(app, support_credentials=True)
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-setuid-sandbox")
+chrome_options.add_argument("--disable-dev-shm-using")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-features=NetworkService")
+chrome_options.add_argument("--headless")
+la_tz = pytz.timezone('America/Los_Angeles')
+chrome_options.add_argument("--user-data-dir=/tmp/foo")
+chrome_options.add_argument(f'--lang=en-US')
+chrome_options.add_argument(f'--timezone="{datetime.now(la_tz).strftime("%z")}"')
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-driver.execute_script("Intl.DateTimeFormat().resolvedOptions().timeZone = 'America/Los_Angeles';")
+# driver.execute_script("Intl.DateTimeFormat().resolvedOptions().timeZone = 'America/Los_Angeles';")
 
 
 # MySql ####################
