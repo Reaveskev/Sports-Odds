fa42c08620fa9c591df086bf88ab3d3aeeacf4e7
Reaveskev <Reaveskev@gmail.com>
2023-05-01 19:00:06 -0700

    adjusting requirements text

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import requests
 import os
-import datetime
+from datetime import datetime
 import pytz
 # import bcrypt
 
